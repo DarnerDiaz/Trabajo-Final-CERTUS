5dbbf63d06c96553f55deb3fdc723730aa2b2c0d
Darner Iszat Diaz Zapana <darner.diazu@gmail.com>
2023-05-14 22:30:16 -0500

    Trabajo terminado

---
+++ js/products.js
@@ -0,0 +1,98 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Polo negro cuello redondo",
+    precio: 27,
+    img:
+      "img/Polo unicolor de cuello redondo hombre.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Polo con estampado hombre",
+    precio: 33,
+    img:
+      "img/Polo con estampado de letra.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Polo con estampado de rayas",
+    precio: 36,
+    img:
+      "img/Polo con estampado de rayas.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Sudadera estampado de mano",
+    precio: 36,
+    img:
+      "img/Sudadera con estampado de mano.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 5,
+    nombre: "Camiseta con estampado de oso ",
+    precio: 33,
+    img:
+      "img/Camiseta con estampado de oso de dibujos animados y letra.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 6,
+    nombre: "Camiseta de hombros caídos ",
+    precio: 30,
+    img:
+      "img/Camiseta de hombros caídos con estampado de slogan y dibujos animados.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 7,
+    nombre: "Camisa formal unicolor con botón delantero",
+    precio: 67,
+    img:
+      "img/Camisa unicolor con botón delantero.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 8,
+    nombre: "Pantalones con bolsillo  de cintura con cordón",
+    precio: 57,
+    img:
+      "img/Pantalones con bolsillo oblicuo de cintura con cordón.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 9,
+    nombre: "Pantalones de traje unicolor con bolsillo oblicuo",
+    precio: 66,
+    img:
+      "img/Pantalones de traje unicolor con bolsillo oblicuo.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 10,
+    nombre: "Pantalones negros de traje con bolsillo oblicuo sin cinturón",
+    precio: 60,
+    img:
+      "img/Pantalones de traje con bolsillo oblicuo sin cinturón.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 11,
+    nombre: "Reloj cuarzo de puntero redondo & 3 piezas Pulsera",
+    precio: 12,
+    img:
+      "img/Reloj cuarzo de puntero redondo & 3 piezas Pulsera.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 12,
+    nombre: "Reloj cuarzo de puntero redondo & 1 pieza Pulsera",
+    precio: 12,
+    img:
+      "img/Reloj cuarzo de puntero redondo & 1 pieza Pulsera.jpg",
+    cantidad: 1,
+  },
+];
